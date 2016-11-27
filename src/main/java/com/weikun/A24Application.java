a96e771b797fed29729bb8704c557b4cd823f4e3
weikun <wk2003119@163.com>
2016-11-27 09:33:57 +0800

    1127

---
+++ src/main/java/com/weikun/A24Application.java
@@ -0,0 +1,12 @@
+package com.weikun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A24Application.class, args);
+	}
+}
